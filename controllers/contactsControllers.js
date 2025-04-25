a70f33222ffa672d803e24851162c1268209d027
Karolina Nikolaienko <mandarinkanasolntse@gmail.com>
2025-04-25 23:33:52 +0200

    realization

--- controllers/contactsControllers.js
+++ controllers/contactsControllers.js
@@ -1,11 +1,67 @@
 import contactsService from "../services/contactsServices.js";
+import HttpError from "../helpers/HttpError.js";
 
-export const getAllContacts = (req, res) => {};
+export const getAllContacts = async (req, res) => {
+  const contacts = await contactsService.listContacts();
+  res.json({
+    status: "success",
+    code: 200,
+    data: {
+      contacts,
+    },
+  });
+};
 
-export const getOneContact = (req, res) => {};
+export const getOneContact = async (req, res) => {
+  const { id } = req.params;
+  const contact = await contactsService.getContactById(id);
+  if (contact)
+    res.json({
+      status: "success",
+      code: 200,
+      data: {
+        contact,
+      },
+    });
+  else throw HttpError(404, `Contact with id=${id} not found`);
+};
 
-export const deleteContact = (req, res) => {};
+export const deleteContact = async (req, res) => {
+  const { id } = req.params;
+  const contact = await contactsService.removeContact(id);
+  if (contact)
+    res.json({
+      status: "success",
+      code: 200,
+      data: {
+        contact,
+      },
+    });
+  else throw HttpError(404, `Contact with id=${id} not found`);
+};
 
-export const createContact = (req, res) => {};
+export const createContact = async (req, res) => {
+  const { name, email, phone } = req.body;
+  const new_contact = await contactsService.addContact(name, email, phone);
+  res.status(201).json(new_contact);
+};
 
-export const updateContact = (req, res) => {};
+export const updateContact = async (req, res) => {
+  const { id } = req.params;
+  const { name, email, phone } = req.body;
+  const updated_contact = await contactsService.updateContact(
+    id,
+    name,
+    email,
+    phone
+  );
+  if (updated_contact)
+    res.json({
+      status: "success",
+      code: 200,
+      data: {
+        contact,
+      },
+    });
+  else throw HttpError(404, `Contact with id=${id} not found`);
+};
--- schemas/contactsSchemas.js
+++ schemas/contactsSchemas.js
@@ -1,5 +1,13 @@
 import Joi from "joi";
 
-export const createContactSchema = Joi.object({});
+export const createContactSchema = Joi.object({
+  name: Joi.name().required(),
+  email: Joi.email().required(),
+  phone: Joi.phone().required(),
+});
 
-export const updateContactSchema = Joi.object({});
+export const updateContactSchema = Joi.object({
+  name: Joi.name(),
+  email: Joi.email(),
+  phone: Joi.phone(),
+});
